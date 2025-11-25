0ad33a850f8165514667e8863c8cb29309300d36
rchrdkvcs <gumimacihun@gmail.com>
2025-11-25 15:12:35 +0100

    feat: implement Players module with controller, service, and DTOs

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { TablesModule } from './tables/tables.module';
+import { PlayersModule } from './players/players.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), TablesModule, PlayersModule],
   controllers: [AppController],
   providers: [AppService],
 })
