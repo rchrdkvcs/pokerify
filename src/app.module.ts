6f830ed69d9f6571cbdeffdc909711999275daac
rchrdkvcs <gumimacihun@gmail.com>
2025-11-25 16:00:15 +0100

    feat: integrate Mongoose for MongoDB support in the application

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,17 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { TablesModule } from './tables/tables.module';
 import { PlayersModule } from './players/players.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ConfigModule.forRoot(), TablesModule, PlayersModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(
+      process.env.MONGODB_URI || 'mongodb://localhost/poker_db',
+    ),
+    TablesModule,
+    PlayersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
