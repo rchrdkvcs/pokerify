748dba8d2e2654da6061164a46598300166d5466
rchrdkvcs <gumimacihun@gmail.com>
2025-11-27 16:40:29 +0100

    feat: integrate Swagger for API documentation and update dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Pokerify API')
+    .setDescription("Documentation pour l'API Pokerify (poker Texas Holdem)")
+    .setVersion('0.0.0-beta.1')
+    .addTag('pokerify')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
