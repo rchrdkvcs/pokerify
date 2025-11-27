8125fb3654aae65815a8215ad437ea817029cdc5
rchrdkvcs <gumimacihun@gmail.com>
2025-11-27 20:23:13 +0100

    feat: implement authentication module with JWT support

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,8 @@ import { ConfigModule } from '@nestjs/config';
 import { TablesModule } from './tables/tables.module';
 import { PlayersModule } from './players/players.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -14,6 +16,8 @@ import { MongooseModule } from '@nestjs/mongoose';
     ),
     TablesModule,
     PlayersModule,
+    AuthModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
